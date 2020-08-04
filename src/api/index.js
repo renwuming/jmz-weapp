f7a9dc77e15b2daeae97ed3d638bb5ec400748da
任天赋 <rentianfu@xiaomi.com>
2020-08-05 01:11:46 +0800

    feat: 上传userInfo到服务器时，增加encryptedData

--- src/api/index.js
+++ src/api/index.js
@@ -1,92 +1,90 @@
-import Taro from '@tarojs/taro'
-import { baseUrl, baseUrlAuth, weappName } from '../config'
-import _ from 'lodash'
+import Taro from '@tarojs/taro';
+import { baseUrl, baseUrlAuth, weappName } from '../config';
+import _ from 'lodash';
 
 function _request(params) {
-  params = handleHeader(params)
+  params = handleHeader(params);
   // 若非http开头的url，则添加baseUrl
-  const { url } = params
+  const { url } = params;
   if (!/^http/.test(url)) {
-    params.url = `${baseUrl}${url}`
+    params.url = `${baseUrl}${url}`;
   }
   return Taro.request(params)
-    .then(res => res.data)
-    .then(res => {
-      const { code, error } = res
+    .then((res) => res.data)
+    .then((res) => {
+      const { code, error } = res;
       if (code > 400) {
         if (code > 500) {
-          warning(error)
+          warning(error);
         }
-        return Promise.reject(error)
+        return Promise.reject(error);
       } else {
-        return res
+        return res;
       }
-    })
+    });
 }
 
 function handleHeader(params) {
-  const ticket = Taro.getStorageSync('ticket')
+  const ticket = Taro.getStorageSync('ticket');
   params = _.merge({}, params, {
     header: {
       'x-ticket': ticket,
-      'x-weappname': weappName
-    }
-  })
-  return params
+      'x-weappname': weappName,
+    },
+  });
+  return params;
 }
 
-export const updateUserInfo = userInfo => {
+export const updateUserInfo = (fullUserInfo) => {
   return _request({
     method: 'POST',
     url: `${baseUrlAuth}/weapp-user/update-userinfo`,
-    data: {
-      userInfo
-    }
-  })
-}
+    data: fullUserInfo,
+  });
+};
 
 export const validate = () => {
   return _request({
     method: 'POST',
-    url: `/users/validate`
+    url: `/users/validate`,
   })
-    .then(res => {
-      const { userInfo, mode, history, isAdmin } = res
+    .then((res) => {
+      const { userInfo, mode, history, isAdmin } = res;
       // 缓存userInfo和mode
-      Taro.setStorageSync('userInfo', userInfo)
-      Taro.setStorageSync('mode', mode)
-      Taro.setStorageSync('history', history)
-      Taro.setStorageSync('isAdmin', isAdmin)
-      return res
+      Taro.setStorageSync('userInfo', userInfo);
+      Taro.setStorageSync('mode', mode);
+      Taro.setStorageSync('history', history);
+      Taro.setStorageSync('isAdmin', isAdmin);
+      return res;
     })
     .catch(() => {
-      return login()
-    })
-}
+      return login();
+    });
+};
 
 const login = () => {
-  return Taro.login().then(res => {
-    const { code } = res
+  return Taro.login().then((res) => {
+    const { code } = res;
     return _request({
       method: 'POST',
       url: `${baseUrlAuth}/weapp-user/login`,
       data: {
-        code
-      }
-    }).then(res => {
-      const { ticket } = res
+        code,
+      },
+    }).then((res) => {
+      const { ticket } = res;
       // 缓存ticket
-      Taro.setStorageSync('ticket', ticket)
-    })
-  })
-}
+      Taro.setStorageSync('ticket', ticket);
+    });
+  });
+};
 
 function warning(title) {
   Taro.showToast({
     title,
     icon: 'none',
-    duration: 3000
-  })
+    duration: 3000,
+  });
 }
 
-export const request = _request
+export const request = _request;
--- src/components/LoginBtn/index.tsx
+++ src/components/LoginBtn/index.tsx
@@ -1,55 +1,55 @@
-import Taro, { Component } from '@tarojs/taro'
-import { View } from '@tarojs/components'
-import { AtButton } from 'taro-ui'
-import './index.scss'
-import { updateUserInfo } from '../../api'
+import Taro, { Component } from '@tarojs/taro';
+import { View } from '@tarojs/components';
+import { AtButton } from 'taro-ui';
+import './index.scss';
+import { updateUserInfo } from '../../api';
 
 interface IProps {
-  className: string
-  text: string
-  callback: Function
+  className: string;
+  text: string;
+  callback: Function;
 }
 
 export default class Index extends Component<IProps, any> {
   static defaultProps = {
     className: '',
     text: '',
-    callback: () => {}
-  }
+    callback: () => {},
+  };
 
-  state = {}
+  state = {};
 
   componentDidHide() {}
   componentWillUnmount() {}
   componentDidShow() {}
 
   getUserInfo(data) {
-    const { detail } = data
-    const { userInfo } = detail
+    const { detail } = data;
+    const { userInfo } = detail;
 
     // 若授权成功
     if (userInfo) {
-      updateUserInfo(userInfo).then(() => {
-        Taro.setStorageSync('userInfo', userInfo)
-        this.props.callback()
-      })
+      updateUserInfo(detail).then(() => {
+        Taro.setStorageSync('userInfo', userInfo);
+        this.props.callback();
+      });
     } else {
-      const delay = 1000
+      const delay = 1000;
       // 若拒绝授权
       Taro.showToast({
         title: '不授权将使用默认昵称、头像，可以稍后重新授权',
         // title: '不授权昵称、头像，无法创建或加入房间',
         icon: 'none',
-        duration: delay
-      })
+        duration: delay,
+      });
       setTimeout(() => {
-        this.props.callback()
-      }, delay)
+        this.props.callback();
+      }, delay);
     }
   }
 
   render() {
-    const { text, className } = this.props
+    const { text, className } = this.props;
     return (
       <View>
         <AtButton
@@ -58,13 +58,13 @@ export default class Index extends Component<IProps, any> {
           type="primary"
           size="normal"
           openType="getUserInfo"
-          onGetUserInfo={data => {
-            this.getUserInfo(data)
+          onGetUserInfo={(data) => {
+            this.getUserInfo(data);
           }}
         >
           {text}
         </AtButton>
       </View>
-    )
+    );
   }
 }
